e657b5576f38d1b46500d6b9a3875bf798ef4e72
Ivan Osipov <ivan.osipov.95@mail.ru>
2015-12-06 21:43:59 +0300

    Perform t01

---
+++ ObjectOrientedProgramming/src/main/java/javase01/t01/Pen.java
@@ -0,0 +1,76 @@
+package javase01.t01;
+
+import java.util.Objects;
+
+public class Pen {
+
+    private double price;
+    private String mark;
+    private String type;
+    private String color;
+
+    public Pen(int price, String mark, String type, String color) {
+        this.price = price;
+        this.mark = mark;
+        this.type = type;
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pen pen = (Pen) o;
+        return Objects.equals(price, pen.price) &&
+                Objects.equals(mark, pen.mark) &&
+                Objects.equals(type, pen.type) &&
+                Objects.equals(color, pen.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mark, type, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Pen {" +
+                "price=" + price +
+                ", mark='" + mark + '\'' +
+                ", type='" + type + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
