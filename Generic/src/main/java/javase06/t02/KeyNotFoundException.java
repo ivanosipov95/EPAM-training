b4f2dc910d45f515d059dbe8ff00b539fe75e90e
Ivan Osipov <ivan.osipov.95@mail.ru>
2016-02-08 11:34:20 +0300

    Perform t02 (Generic)

---
+++ Generic/src/main/java/javase06/t02/KeyNotFoundException.java
@@ -0,0 +1,9 @@
+package javase06.t02;
+
+
+public class KeyNotFoundException  extends Exception{
+
+    public KeyNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ Generic/src/main/java/javase06/t02/Reader.java
@@ -0,0 +1,53 @@
+package javase06.t02;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Ответьте на вопрос: как ведет себя map-коллекция если в нее добавить элемент с ключом, который уже присутствует?
+ * значение по этому ключу перезапишется!
+ */
+
+public class Reader {
+
+    private Map<String, String> map;
+    private String inPath;
+
+    public Reader(String inPath) throws FileNotFoundException, KeyNotFoundException {
+        this.inPath = inPath;
+        this.map = getMap(this.inPath);
+    }
+
+    public String getValue(String key) throws KeyNotFoundException {
+        for (String s : map.keySet()) {
+            if (!s.equals(key)) {
+                throw new KeyNotFoundException("Can't find key");
+            }
+        }
+        return map.get(key);
+    }
+
+    private Map<String, String> getMap(String inPath) throws FileNotFoundException, KeyNotFoundException {
+        String line = null;
+        String[] splitLine;
+        map = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inPath))) {
+            while ((line = reader.readLine()) != null) {
+                splitLine = line.split("=");
+                if (splitLine[0].equals("")) {
+                    throw new KeyNotFoundException("Can't find key");
+                }
+                map.put(splitLine[0], splitLine[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return map;
+    }
+}
